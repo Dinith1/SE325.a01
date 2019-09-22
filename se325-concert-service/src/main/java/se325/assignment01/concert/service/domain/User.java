bfbc3b51860d1eb63b1870d93c13fdb77e621992
Dinith1 <wannigamadinith@gmail.com>
2019-09-22 13:17:54 +1200

    added User class

---
+++ se325-concert-service/src/main/java/se325/assignment01/concert/service/domain/User.java
@@ -0,0 +1,85 @@
+package se325.assignment01.concert.service.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this(null, username, password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("User, id: ");
+        buffer.append(id);
+        buffer.append(", username: ");
+        buffer.append(username);
+        buffer.append(", password: ");
+        buffer.append(password);
+        return buffer.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof User))
+            return false;
+
+        if (obj == this)
+            return true;
+
+        User user = (User) obj;
+        return new EqualsBuilder().append(username, user.getUsername()).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 31).append(username).hashCode();
+    }
+
+}
